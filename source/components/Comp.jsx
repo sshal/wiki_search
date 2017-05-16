75e3eacf4a190f06f46985e60e777b28de151cac
Shalimov Sergey <shalimovser90@gmail.com>
2017-05-16 12:12:41 +0300

    Initial commit

---
+++ source/components/Comp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Comp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>Example</div>
+        )
+    }
+}
+
+module.exports = Comp;
---
+++ source/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Comp from './components/Comp.jsx';
+
+ReactDOM.render(<Comp />, document.getElementById('main'));
